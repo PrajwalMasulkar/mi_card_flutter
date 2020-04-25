83330c96d7555dec97887b7dc03658795f7926ad
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-04-25 21:41:13 +0530

    'new'

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,7 @@ class MyApp extends StatelessWidget {
                ),
              ),
              Container(
+
                padding: EdgeInsets.all(10.0),
                color: Colors.white,
                margin: EdgeInsets.symmetric(vertical: 10.0,
@@ -57,6 +58,31 @@ class MyApp extends StatelessWidget {
                      fontFamily: 'Sorce Sans Pro',
                      fontSize: 20.0,
                    ),)
+                 ],
+               ),
+             ),
+             Container(
+               color: Colors.white,
+               padding: EdgeInsets.all(10.0),
+               margin: EdgeInsets.symmetric(vertical: 10.0,
+               horizontal: 25.0),
+               child: Row(
+                 children: <Widget>[
+                   Icon(
+                       Icons.email,
+                   color: Colors.teal,
+                   ),
+                   SizedBox(
+                     width: 10.0,
+                   ),
+                   Text(
+                     'prajwalmasulkar75@gmail.com',
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.black,
+                       fontFamily: 'Source Sans Pro'
+                     ),
+                   )
                  ],
                ),
              )
