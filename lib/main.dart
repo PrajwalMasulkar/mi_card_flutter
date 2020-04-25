133ae403049e507836696fb8cb7fbfd23d00bcd7
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-04-25 20:10:35 +0530

    'new'

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+         child: Column(
+           children: <Widget>[
+             CircleAvatar(
+               radius: 50.0,
+               backgroundImage: AssetImage('images/ac.jpg'),
+             ),
+             Text(
+               'Prajwal Masulkar',
+               style: TextStyle(
+                 fontFamily: 'Pacifico',
+                 fontSize: 40.0,
+                 color: Colors.white,
+                 fontWeight: FontWeight.bold,
+               ),
+             ),
+             Text(
+               'FLUTTER DEVELOPER',
+               style: TextStyle(
+                 fontFamily: 'Source Sans Pro',
+                 color: Colors.teal.shade100,
+                 fontSize: 20.0,
+                 letterSpacing: 2.5,
+                 fontWeight: FontWeight.bold,
+               ),
+             )
+           ],
+         ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
